19ce4222e614ca72dff530118b55070e0e2f843c
Felicia Amanda <felicia.amanda@tokopedia.com>
2021-03-30 14:15:14 +0700

    move downsize image

--- Sources/XCTestHTMLReport/main.swift
+++ Sources/XCTestHTMLReport/main.swift
@@ -21,11 +21,12 @@ var result = ValueArgument(.path, "r", "resultBundlePath", required: true, allow
 var renderingMode = Summary.RenderingMode.linking
 var inlineAssets = BlockArgument("i", "inlineAssets", required: false, helpMessage: "Inline all assets in the resulting html-file, making it heavier, but more portable") {
     renderingMode = .inline
-}
-var downsizeImagesEnabled = true
-var downsizeImages = BlockArgument("z", "downsize-images", required: false, helpMessage: "Downsize image screenshots") {
     downsizeImagesEnabled = true
 }
+// var downsizeImagesEnabled = true
+// var downsizeImages = BlockArgument("z", "downsize-images", required: false, helpMessage: "Downsize image screenshots") {
+//     downsizeImagesEnabled = true
+// }
 var deleteUnattachedFilesEnabled = false
 var deleteUnattachedFiles = BlockArgument("d", "delete-unattached", required: false, helpMessage: "Delete unattached files from bundle, reducing bundle size") {
     deleteUnattachedFilesEnabled = true
@@ -35,7 +36,7 @@ var deleteUnattachedFiles = BlockArgument("d", "delete-unattached", required: fa
 command.arguments = [help,
                      verbose,
                      junit,
-                     downsizeImages,
+                    //  downsizeImages,
                      deleteUnattachedFiles,
                      result,
                      inlineAssets]
